860c776c412b7c14bea6ab08fe476b453471d275
Anthony Moss <asmoss93@gmail.com>
2019-04-10 13:16:03 -0400

    adds structure to get app running on server

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 require('dotenv').config();
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
+app.use(session({
+    store: new FileStore(),  // no options for now
+    secret: process.env.SESSION_SECRET
+}));
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Do The Thing</h1>
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`)
+});
