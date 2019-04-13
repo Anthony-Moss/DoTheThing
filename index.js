c5ff5f86f56a50ddd4f31dc829871b560eca05fd
chastity duhaney <chastity_duhaney82@gmail.com>
2019-04-13 04:30:44 -0400

    added required routes, controlas and models to connect all pending.js files

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const session = require("express-session");
 const FileStore = require("session-file-store")(session);
 const AllTickets = require("./models/allTickets");
 const OpenTickets = require("./models/openTickets");
+const pendingTickets = require("./models/pendingTickets");
 app.use(express.urlencoded({ extended: true }));
 app.use(
-    session({
+  session({
     store: new FileStore(), // no options for now
     secret: process.env.SESSION_SECRET
-    })
+  })
 );
 app.engine("html", es6Renderer);
 app.set("view engine", "html");
@@ -22,12 +23,14 @@ const loginRoutes = require("./routes/login");
 const dashboardRoutes = require("./routes/dashboard");
 const allTicketsRoutes = require("./routes/allTickets");
 const openTicketsRoutes = require("./routes/openTickets");
+const pendingTicketsRoutes = require("./routes/pendingTickets");
 
 app.use("/login", loginRoutes);
 app.use("/dashboard", dashboardRoutes);
 app.use("/allTickets", allTicketsRoutes);
 app.use("/openTickets", openTicketsRoutes);
+app.use("/pendingTickets", pendingTickets);
 
 app.listen(PORT, () => {
-    console.log(`server is running at port: ${PORT}`);
+  console.log(`server is running at port: ${PORT}`);
 });
