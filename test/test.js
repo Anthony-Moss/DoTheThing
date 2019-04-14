baafcdf8b45902885f4c9f532f805f6933df9d80
Anthony Moss <asmoss93@gmail.com>
2019-04-14 06:05:32 -0400

    adds one more test function for encrypt password, still broken, if I check this commit tomorrow I think I need to start fresh I dont think I have the setpasword function working or setup correctly anymore because all changes

--- test/test.js
+++ test/test.js
@@ -9,9 +9,8 @@ describe('User model', function () {
     it('should be able to retrieve by id', async() => {
         const theUser = await User.getById(1);
         theUser.should.be.an.instanceOf(User);
-        // theUser.should.have.length(1);
     });
-    
+
     it('should error if no user by id', async ()=> {
         const theUser = await User.getById(27645);
         expect(theUser).to.be.null;
@@ -21,27 +20,25 @@ describe('User model', function () {
         const theUser = await User.getById(1);
         theUser.email = 'new@new.com';
         await theUser.save();
-
-        // re-grab the user with id 2
         const alsoTheUser = await User.getById(2);
-        // // expect the email to be equal to the new value
         expect(theUser.email).to.equal('new@new.com');
     });
 
     it ('should be able to check for correct passwords', async () => {
-        //get a user with id 1
         const theUser = await User.getById(1);
-        // set their password field to "bacon"
         theUser.setPassword("bacon");
-        // save them to the database
         await theUser.save();
-        //get them back out of the databse
         const sameUser = await User.getById(1);
-        //ask them if their password is "bacon"
-        const isCorrectPassword = theUser.checkPassword("bacon");
+        const isCorrectPassword = theUser.checkPassword("waffles");
         expect(isCorrectPassword).to.be.true;
 
-        const isNotCorrectPassword = sameUser.checkPassword('antibacon');
+        const isNotCorrectPassword = sameUser.checkPassword('notwaffles');
         expect(isNotCorrectPassword).to.be.false;
+    });
+
+    it('should encrypt the password', async () => {
+        const theUser = await User.getById(1);
+        theUser.setPassword("password");
+        expect (theUser.password).not.to.equal('password');
     });
 });
