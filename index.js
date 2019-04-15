cb415c6c62cd084dbbb98dec520a53c41fa30935
chastity duhaney <chastity_duhaney82@gmail.com>
2019-04-15 14:25:56 -0400

    trying to fix on sms messaging

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const PORT = process.env.PORT;
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const Nexmo = require("nexmo");
-const socketio = require("socket.io");
 const app = express();
 const es6Renderer = require("express-es6-template-engine");
 const session = require("express-session");
@@ -37,10 +35,9 @@ const pendingTicketsRoutes = require("./routes/pendingTickets");
 const completedTicketsRoutes = require("./routes/completedTickets");
 const detailsRoutes = require("./routes/details");
 const createUserRoutes = require("./routes/createUser");
-const nexmo = new Nexmo({
-  apiKey: "b9dbc1cb",
-  apiSecret: "nX9TJeg6zs4AzOgo"
-}, { debug: true });
+
+
+
 
 app.use("/login", loginRoutes);
 app.use("/dashboard", dashboardRoutes);
@@ -53,55 +50,27 @@ app.use('/', homepageRoutes);
 app.use('/request', requestRoutes);
 app.use('/createUser', createUserRoutes);
 
-// Index value for SMS Message
-app.get('/', (req, res) => {
-  res.render('completed')
-});
 
-// Catch form submit SMS messages
-app.post('/', (req, res) => {
-  //res.send(req.body);
-  //console.log(req.body);
-  const number = req.body.number;
-  const text = req.body.text;
+// Emit to the client
+//io.emit('smsStatus', data);
 
-  nexmo.message.sendSms(
-    '17576933212', number, text, { type: 'unicode' },
-    (err, responseData) => {
-      if (err) {
-        console.log(err);
-
-      } else {
-        console.dir(responseData);
-        // Get data from response
-        const data = {
-          id: responseData.messages[0]['message-id'],
-          number: responseData.messages[0]['to']
-        }
-        // Emit to the client
-        io.emit('smsStatus', data);
-      }
-    }
-  );
-});
 
 // THIS LETS US SERVE IMAGES & THE CSS FILE
 app.use('/static', express.static('static'));
-app.use(express.static(__dirname + '/smsMessages'));
+//app.use(express.static(  + '/smsMessages'));
 
 
 app.listen(PORT, () => {
   console.log(`server is running at port: ${PORT}`);
   // Connect to socket.io
-  const io = socketio();
-  io.on('connection', (socket) => {
-    console.log('Connected');
-    io.on('disconnect', () => {
-      console.log('Disconnected');
+  // const io = socketio();
+  // io.on('connection', (socket) => {
+  //   console.log('Connected');
+  //   io.on('disconnect', () => {
+  //     console.log('Disconnected');
 
-    })
-  });
 });
+
 
 
 
--- routes/completedTickets.js
+++ routes/completedTickets.js
@@ -1,8 +1,44 @@
 const express = require("express");
+const app = express();
 const Router = express.Router;
 const completedTicketsRoutes = Router();
+const Nexmo = require('nexmo');
+const nexmo = new Nexmo({
+    apiKey: "b9dbc1cb",
+    apiSecret: "nX9TJeg6zs4AzOgo"
+}, { debug: true });
 
 const { completedTicketsPage } = require("../controllers/completedTickets");
 
 completedTicketsRoutes.get("/", completedTicketsPage);
-module.exports = completedTicketsRoutes;
+//Index value for SMS Message
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+// Catch form submit SMS messages
+app.post('/', (req, res) => {
+    //res.send(req.body);
+    //console.log(req.body);
+    const number = req.body.number;
+    const text = req.body.text;
+
+    nexmo.message.sendSms(
+        '17576933212', number, text, { type: 'unicode' },
+        (err, responseData) => {
+            if (err) {
+                console.log(err);
+
+            } else {
+                console.dir(responseData);
+                // Get data from response
+                //const data = {
+                //id: responseData.messages[0]['message-id'],
+                //number: responseData.messages[0]['to']
+            }
+        }
+    );
+});
+module.exports =
+    completedTicketsRoutes;
+
